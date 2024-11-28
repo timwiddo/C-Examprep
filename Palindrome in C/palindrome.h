fac880646392b7383b3eeb1f801f5c27a4852c5e
Tim Widmoser <82330733+timwiddo@users.noreply.github.com>
2024-11-28 18:23:58 +0100

    Add files via upload

---
+++ Palindrome in C/palindrome.h
@@ -0,0 +1,12 @@
+//
+// Created by Tim on 28/11/2024.
+//
+
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+char *reverse(char *str);
+void ispalindrome(char *str);
+void pointer_ispalindrome(char *str);
+
+#endif //PALINDROME_H
---
+++ Palindrome in C/palindrome_main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "palindrome.h"
+//Palindrome Check
+
+
+
+int main(void) {
+    ispalindrome("abc");
+    ispalindrome("otto");
+    ispalindrome("Otto");
+    ispalindrome("madam");
+    pointer_ispalindrome("hallo");
+    pointer_ispalindrome("otto");
+}
+
